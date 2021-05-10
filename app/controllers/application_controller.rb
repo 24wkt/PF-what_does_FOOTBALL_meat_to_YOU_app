05c1b42330b2ac61f67f65f1e2827a4e9b57cad6
24wkt <05wkt38xy.waku@gmail.com>
2021-05-10 10:38:02 +0000

    [update]deviseのviewとcontrollerを編集

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    # エンドユーザー側の新規登録情報時に下記項目の操作を許可
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email])
+    # 管理者側とエンドユーザー側のログイン時に下記項目の操作を許可
+    devise_parameter_sanitizer.permit(:sign_in, keys: [:email])
+  end
 end
