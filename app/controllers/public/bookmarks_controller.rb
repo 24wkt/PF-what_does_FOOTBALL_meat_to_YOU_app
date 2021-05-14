91efb24eba63e56c81dcc2a5df959692eb68d7aa
24wkt <05wkt38xy.waku@gmail.com>
2021-05-14 02:52:06 +0000

    [add]ブックマーク機能画面

--- app/controllers/public/bookmarks_controller.rb
+++ app/controllers/public/bookmarks_controller.rb
@@ -8,7 +8,7 @@ class Public::BookmarksController < ApplicationController
 
   def destroy
     @blog = Blog.find(params[:blog_id])
-    bookmark = current_end_user.bookmarks.find_by(blog_id: @blog.id, current_end_user_id: current_end_user.id)
+    bookmark = current_end_user.bookmarks.find_by(blog_id: @blog.id, end_user_id: current_end_user.id)
     bookmark.destroy
   end
 
