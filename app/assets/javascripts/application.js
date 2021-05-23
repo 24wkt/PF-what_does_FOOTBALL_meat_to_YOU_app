c1b0283260663f7eb8768c6e1f8a11efa1113d2c
24wkt <05wkt38xy.waku@gmail.com>
2021-05-23 06:16:05 +0000

    [update]

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -30,7 +30,7 @@ $(function() {
         // ↓現在アクティブなタブの中身を非表示
         $('.box-show').removeClass('box-show');
         // ↓クリックしたタブから順番を取得↓
-        const index = $(this).index();
+        var index = $(this).index();
         // ↓クリックしたタブと同じ順番のコンテンツを表示↓
         $('.tabbox').eq(index).addClass('box-show');
     });
