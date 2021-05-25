935c2c9035bfa75a3f0f4c0a86e6f32aea43079a
24wkt <05wkt38xy.waku@gmail.com>
2021-05-25 12:24:11 +0000

    [add]管理者側ログイン保持

--- app/controllers/public/fronts_controller.rb
+++ app/controllers/public/fronts_controller.rb
@@ -2,6 +2,8 @@ class Public::FrontsController < ApplicationController
   def top
     if current_end_user
       redirect_to end_users_home_path
+    elsif current_admin
+      redirect_to admin_top_path
     end
   end
 
